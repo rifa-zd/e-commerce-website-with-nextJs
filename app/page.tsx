784c1019509de6bc7250c10e9b1a979ee1eb60d3
rifa-zd <rifazad1w@gmail.com>
2026-04-16 21:22:59 +0600

    shadcn, color set, a complete mess

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
@@ -8,14 +11,22 @@ export default function Home() {
             {/* hero heading  */}
             <p className="text-black mb-6 text-4xl font-bold">
               For every version of{" "}
-              <b>  
+              <b>
                 <i>She</i>
               </b>
             </p>
 
-            <div className="fon">
-              <button>Join Today</button>
-            </div>
+            {/* <div>
+              <Button size={"xs"} className={"h-12 px-8 text-lg font-medium"}>
+                Join Today
+              </Button>
+            </div> */}
+          </div>
+          
+          <div className="flex justify-center gap-6 overflow-hidden">
+            <Badge variant={"custom"}>Free Shipping on Orders Over $75</Badge>
+            <Badge variant={"custom"}>30-Day Hassle-Free Returns</Badge>
+            <Badge variant={"custom"}>Dermatologist Tested Skin Products</Badge>
           </div>
         </section>
       </main>
